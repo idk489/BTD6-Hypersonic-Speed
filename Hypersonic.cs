3e40bcaa41b8940b6b7fd9640990cd159c89f3b3
idk489 <bakerliam282@gmail.com>
2024-05-05 12:36:50 -0400

    Renamed to Hypersonic

--- Hypersonic.cs
+++ Hypersonic.cs
@@ -1,20 +1,20 @@
 ﻿using MelonLoader;
 using BTD_Mod_Helper;
-using TemplateMod;
+using Hypersonic;
 using Il2CppAssets.Scripts.Models;
 using Il2CppAssets.Scripts.Models.Towers.Weapons;
 using BTD_Mod_Helper.Extensions;
 
-[assembly: MelonInfo(typeof(TemplateMod.TemplateMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonInfo(typeof(Hypersonic.Hypersonic), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
-namespace TemplateMod;
+namespace Hypersonic;
 
-public class TemplateMod : BloonsTD6Mod
+public class Hypersonic : BloonsTD6Mod
 {
     public override void OnApplicationStart()
     {
-        ModHelper.Msg<TemplateMod>("TemplateMod loaded!");
+        ModHelper.Msg<Hypersonic>("Hypersonic loaded!");
     }
     public override void OnNewGameModel(GameModel result)
     {
--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,13 +1,13 @@
-﻿namespace TemplateMod;
+﻿namespace Hypersonic;
 
 public static class ModHelperData
 {
     public const string WorksOnVersion = "42.0";
     public const string Version = "1.0.0";
-    public const string Name = "TemplateMod";
+    public const string Name = "Hypersonic";
 
-    public const string Description = "An empty mod";
+    public const string Description = "makes towers attack really fast";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "idk489"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "BTD6-Hypersonic-Speed"; // TODO add your repo name here, also in the download url in README.md
 }
