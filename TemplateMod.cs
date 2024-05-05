14a16372dc18141a70e951846a818ac03530c270
idk489 <bakerliam282@gmail.com>
2024-05-05 00:57:44 -0400

    Add project files.

---
+++ TemplateMod.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using BTD_Mod_Helper;
+using TemplateMod;
+using Il2CppAssets.Scripts.Models;
+using Il2CppAssets.Scripts.Models.Towers.Weapons;
+using BTD_Mod_Helper.Extensions;
+
+[assembly: MelonInfo(typeof(TemplateMod.TemplateMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace TemplateMod;
+
+public class TemplateMod : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<TemplateMod>("TemplateMod loaded!");
+    }
+    public override void OnNewGameModel(GameModel result)
+    {
+        foreach(var weapon in result.GetDescendants<WeaponModel>().ToList())
+        {
+            weapon.rate = 0;
+        }
+    }
+}
